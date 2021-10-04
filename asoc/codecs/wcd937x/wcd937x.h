a1135f1285b34b4e2b5d2103eee5f9f70aac67ce
Sairam Peri  <peri@codeaurora.org>
2021-10-04 13:52:04 +0530

    asoc: codecs: wcd937x codec driver update for waipio machine driver support
    Update the codec driver wcd937x to make it compatible with the
    waipio machine driver.
    
    Change-Id: I66baeb4ed2b50ea3a8f66f87a9549269d4fd296e
    Signed-off-by: Sairam Peri <peri@codeaurora.org>
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- asoc/codecs/wcd937x/wcd937x.h
+++ asoc/codecs/wcd937x/wcd937x.h
@@ -52,6 +52,8 @@ static int wcd937x_swr_master_ch_map[] = {
 extern int wcd937x_info_create_codec_entry(struct snd_info_entry *codec_root,
 				    struct snd_soc_component *component);
 
+extern int wcd937x_get_codec_variant(struct snd_soc_component *component);
+
 static inline int wcd937x_slave_get_master_ch_val(int ch)
 {
 	int i;
@@ -80,6 +82,10 @@ static inline int wcd937x_slave_get_slave_ch_val(int ch)
 #else
 extern int wcd937x_info_create_codec_entry(struct snd_info_entry *codec_root,
 				    struct snd_soc_component *component)
+{
+	return 0;
+}
+static inline int wcd937x_get_codec_variant(struct snd_soc_component *component)
 {
 	return 0;
 }
