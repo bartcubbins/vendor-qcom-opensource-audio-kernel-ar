b127b9e03a9abbcda5ebb643e0855c3d7cfd8c5e
Pavel Dubrova <pashadubrova@gmail.com>
2026-05-07 05:38:53 +0300

    ASoC: Fix formatting issues
    Various changes to fix format specifiers in Waipio machine driver,
    WCD937x and WSA881x codecs.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- asoc/waipio.c
+++ asoc/waipio.c
@@ -1437,7 +1437,7 @@ static int msm_snd_card_late_probe(struct snd_soc_card *card)
 	if (!rtd) {
 		dev_err(card->dev,
 			"%s: snd_soc_get_pcm_runtime for %s failed!\n",
-			__func__, card->dai_link[0]);
+			__func__, card->dai_link[0].name);
 		return -EINVAL;
 	}
 
@@ -2051,7 +2051,7 @@ static int waipio_ssr_enable(struct device *dev, void *data)
 	if (!rtd_wcd) {
 		dev_dbg(dev,
 			"%s: snd_soc_get_pcm_runtime for %s failed!\n",
-			__func__, card->dai_link[0]);
+			__func__, card->dai_link[0].name);
 	}
 
 	if (pdata->wsa_max_devs > 0) {
@@ -2064,7 +2064,7 @@ static int waipio_ssr_enable(struct device *dev, void *data)
 		if (!rtd_wsa) {
 			dev_dbg(dev,
 			"%s: snd_soc_get_pcm_runtime for %s failed!\n",
-			__func__, card->dai_link[ARRAY_SIZE(msm_rx_tx_cdc_dma_be_dai_links)]);
+			__func__, card->dai_link[ARRAY_SIZE(msm_rx_tx_cdc_dma_be_dai_links)].name);
 		}
 	}
 	/* set UPD configuration */
@@ -2196,7 +2196,7 @@ static int msm_asoc_parse_soundcard_name(struct platform_device *pdev,
 		goto parse;
 	}
 	if (len <= 0 || len > sizeof(u32)) {
-		dev_dbg(&pdev->dev, "%s: nvmem cell length out of range: %d\n",
+		dev_dbg(&pdev->dev, "%s: nvmem cell length out of range: %zu\n",
 			__func__, len);
 		kfree(buf);
 		goto parse;
